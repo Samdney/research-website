c00dcaf76a1323de3875fddc26a68fa343a1275d
Carolin Zöbelein <contact@carolin-zoebelein.de>
2021-04-12 23:18:17 +0200

    some projects added

--- projectsbib.py
+++ projectsbib.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # TODO: Grants
 # TODO: Misc Related
 
-PROJECTSBIB_LASTUPDATE = '2021/04/06'
+PROJECTSBIB_LASTUPDATE = '2021/04/12'
 
 PROJECTSBIB = ( 
 #    ('', '', '', '', '', '', '', '', '', (
@@ -22,7 +22,21 @@ PROJECTSBIB = (
 #            ('OSF', 'subtitle01_01', 'sublink01_01', 'substatus01_01', 'authors01_01', 'period01_01', 'subid01_01', 'subdoi01_01', 'readmore01_01')            
 #        )
 #    )
+    ('Globe', 'TODO :)', '', '', '---', '', '', '', '', (
+            ('', '', '', '', '', '', '', '', ''),
+            ('', '', '', '', '', '', '', '', '')
+        )
+    ),
+    ('Globe', 'Bit patterns of elementary number theory problems', '', '', '---', '', '', '', '', (
+            ('', '', '', '', '', '', '', '', ''),
+            ('', '', '', '', '', '', '', '', '')
+        )
+    ),
     ('Globe', 'Dynamical algorithms on hypergraphs', '', 'https://osf.io/6eps2/', 'Open', 'Since 2021/02', '', 'https://doi.org/10.17605/OSF.IO/6EPS2', '', (
+            ('OSF', 'Data flow blocking circumvention by dynamical hypergraph network protocol quantization (Quantum mechanics)', '', '<b><font style="color: red;">Grant Search</font></b>, Writing background research papers', 'Carolin Zöbelein', '', '', '', ''),
+            ('OSF', 'Data distribution compression/modification in distributed networks by bit flip dynamical hypergraph intrusion', '', '<b><font style="color: red;">Grant Search</font></b>, Writing background research papers', 'Carolin Zöbelein', '', '', '', ''),
+            ('OSF', 'Binary tree data compression on dynamical hypergraphs induced Riemannian manifolds', '', 'Open/Grant Search', 'Carolin Zöbelein', '', '', '', ''),
+            ('OSF', 'Geometrically data flow on dynamical hypergraphs induced Riemannian manifolds', '', 'Open', 'Carolin Zöbelein', '', '', '', ''),
             ('OSF', 'Equilibrium states of graph properties', 'https://osf.io/kdnu7/', 'Open', 'Carolin Zöbelein', '', '', 'https://doi.org/10.17605/OSF.IO/KDNU7', ''),
             ('OSF', 'Decomposition by clique separators', 'https://osf.io/nmjr5/', 'Open', 'Carolin Zöbelein', '', '', 'https://doi.org/10.17605/OSF.IO/NMJR5', '')            
         )
