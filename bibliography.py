85bbc966110977cc92024129741b13ee384cca5e
Carolin Zöbelein <contact@carolin-zoebelein.de>
2021-08-28 08:03:49 +0200

    additional paper info [4] removed

--- bibliography.py
+++ bibliography.py
@@ -12,14 +12,14 @@ from datetime import datetime
 # TODO: Add missing entry information
 # TODO: Remove old subpages
 
-BIBLIOGRAPHY_LASTUPDATE = '2021/03/28'
+BIBLIOGRAPHY_LASTUPDATE = '2021/08/28'
 
 BIBLIOGRAPHY = (
     #('nb', 'title', 'sum', 'author', 'date', 'year', 'kind', 'keywords', 'subjectclass', 'subjects', 'publication', 'publicationlink', 'preprint', 'latex', 'source', 'pdf', 'related', 'misc', 'doi', 'id'),
 
     ('5', 'Dynamical decomposition by clique separators on hypergraphs', '', 'Carolin Zöbelein', '', '2021', 'Preprint', '', '', 'Math, CS', '', '', '', '', '', '', '', '', '', ''),
 
-    ('4', 'Primitive data structures for the 3SUM and k-SUM problem', '', 'Carolin Zöbelein', '', '2021', 'Preprint', '', '', 'Math, CS', 'Submitted to journal', '', '', '', '', '', '', '', '', ''),
+    ('4', 'Primitive data structures for the 3SUM and k-SUM problem', '', 'Carolin Zöbelein', '', '2021', 'Preprint', '', '', 'Math, CS', '', '', '', '', '', '', '', '', '', ''),
 
     ('3', 'BlueTransience', 'A Bluetooth based, short memory, social media mobile network app', 'Carolin Zöbelein', 'March 26, 2021', '2020', 'Proposal', ' Bluetooth, Mobile App, Social Media, Natural Disaster, Political Demonstrations, Internet Shutdown', '', 'CS', ' Authorea', 'https://www.authorea.com/users/402885/articles/515244-proposal-bluetransience', '', 'https://github.com/Samdney/proposal-bluetransience', '', '', '', '', 'https://doi.org/10.22541/au.161676845.56300213/v1', ''),
 
