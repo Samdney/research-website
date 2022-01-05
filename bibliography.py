8c1b158824176ffade90f3856cf49fe4e67042c2
Carolin Zöbelein <contact@carolin-zoebelein.de>
2022-01-05 13:48:16 +0100

    added id: proposal_0001

--- bibliography.py
+++ bibliography.py
@@ -25,7 +25,7 @@ BIBLIOGRAPHY = (
 
     ('4', 'Primitive data structures for the 3SUM and k-SUM problem', '', 'Carolin Zöbelein', '', '2021', 'Preprint', '', '', 'Math, CS', '', '', '', '', '', '', '', '', '', ''),
 
-    ('3', 'BlueTransience', 'A Bluetooth based, short memory, social media mobile network app', 'Carolin Zöbelein', 'March 26, 2021', '2020', 'Proposal', ' Bluetooth, Mobile App, Social Media, Natural Disaster, Political Demonstrations, Internet Shutdown', '', 'CS', ' Authorea', 'https://www.authorea.com/users/402885/articles/515244-proposal-bluetransience', '', 'https://github.com/Samdney/proposal-bluetransience', '', '', '', '', 'https://doi.org/10.22541/au.161676845.56300213/v1', ''),
+    ('3', 'BlueTransience', 'A Bluetooth based, short memory, social media mobile network app', 'Carolin Zöbelein', 'March 26, 2021', '2020', 'Proposal', ' Bluetooth, Mobile App, Social Media, Natural Disaster, Political Demonstrations, Internet Shutdown', '', 'CS', ' Authorea', 'https://www.authorea.com/users/402885/articles/515244-proposal-bluetransience', '', 'https://github.com/Samdney/proposal-bluetransience', '', '', '', '', 'https://doi.org/10.22541/au.161676845.56300213/v1', 'proposal_0001'),
 
     ('2', 'About the proof of the Collatz conjecture', 'I want to show one possibility to proof the Collatz conjecture, also called 3n+1 conjecture, for any natural number N.', 'Carolin Zöbelein', '', '2013', 'Preprint', '', '2010 Mathematics. Primary 11N05', 'Math', '', '', 'https://arxiv.org/abs/1303.2073', '', '', '', '', '', '', 'paper_0001'),
 
